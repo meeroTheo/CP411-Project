95587c631e3c9a9010712c9ba8ebe56d7a3cd9d8
Thenu1200 <thenukan1200@gmail.com>
2023-12-01 14:50:11 -0500

    Update Cube.cpp

--- src/Cube.cpp
+++ src/Cube.cpp
@@ -14,15 +14,17 @@ extern RenderMode renderMode;
 
 Cube::Cube()
 {
-    vertex[0][0] = -1; vertex[0][1] = -1; vertex[0][2] = -1;
-    vertex[1][0] = -1; vertex[1][1] = 1;  vertex[1][2] = -1;
-    vertex[2][0] = 1;  vertex[2][1] = 1;  vertex[2][2] = -1;
-    vertex[3][0] = 1;  vertex[3][1] = -1; vertex[3][2] = -1;
-    vertex[4][0] = -1; vertex[4][1] = -1; vertex[4][2] = 1;
-    vertex[5][0] = -1; vertex[5][1] = 1;  vertex[5][2] = 1;
-    vertex[6][0] = 1;  vertex[6][1] = 1;  vertex[6][2] = 1;
-    vertex[7][0] = 1;  vertex[7][1] = -1; vertex[7][2] = 1;
+    // Adjust vertex positions for a card-like rectangle
+    vertex[0][0] = -1.5; vertex[0][1] = -2; vertex[0][2] = 0;
+    vertex[1][0] = -1.5; vertex[1][1] = 2;  vertex[1][2] = 0;
+    vertex[2][0] = 1.5;  vertex[2][1] = 2;  vertex[2][2] = 0;
+    vertex[3][0] = 1.5;  vertex[3][1] = -2; vertex[3][2] = 0;
+    vertex[4][0] = -1.5; vertex[4][1] = -2; vertex[4][2] = 0.1; // Slightly higher for visibility
+    vertex[5][0] = -1.5; vertex[5][1] = 2;  vertex[5][2] = 0.1;
+    vertex[6][0] = 1.5;  vertex[6][1] = 2;  vertex[6][2] = 0.1;
+    vertex[7][0] = 1.5;  vertex[7][1] = -2; vertex[7][2] = 0.1;
 
+    // Define faces as per the new vertex positions
     face[0][0] = 0; face[0][1] = 1; face[0][2] = 2; face[0][3] = 3;
     face[1][0] = 7; face[1][1] = 6; face[1][2] = 5; face[1][3] = 4;
     face[2][0] = 0; face[2][1] = 4; face[2][2] = 5; face[2][3] = 1;
@@ -30,37 +32,26 @@ Cube::Cube()
     face[4][0] = 3; face[4][1] = 2; face[4][2] = 6; face[4][3] = 7;
     face[5][0] = 0; face[5][1] = 3; face[5][2] = 7; face[5][3] = 4;
 
-	faceNormal[0][0] =  0.0, faceNormal[0][1] =  0.0, faceNormal[0][2] = -1.0,
-	faceNormal[1][0] =  0.0, faceNormal[1][1] =  0.0, faceNormal[1][2] =  1.0;
-	faceNormal[2][0] = -1.0, faceNormal[2][1] =  0.0, faceNormal[2][2] =  0.0;
-	faceNormal[3][0] =  0.0, faceNormal[3][1] =  1.0, faceNormal[3][2] =  0.0;
-	faceNormal[4][0] =  1.0, faceNormal[4][1] =  0.0, faceNormal[4][2] =  0.0;
-	faceNormal[5][0] =  0.0, faceNormal[5][1] = -1.0, faceNormal[5][2] =  0.0;
+    // Update vertex colors for the card-like rectangle
+    vertexColor[0][0] = 1.0; vertexColor[0][1] = 0.0; vertexColor[0][2] = 0.0;
+    vertexColor[1][0] = 0.0; vertexColor[1][1] = 1.0; vertexColor[1][2] = 0.0;
+    vertexColor[2][0] = 0.0; vertexColor[2][1] = 0.0; vertexColor[2][2] = 1.0;
+    vertexColor[3][0] = 1.0; vertexColor[3][1] = 1.0; vertexColor[3][2] = 0.0;
+    vertexColor[4][0] = 1.0; vertexColor[4][1] = 0.0; vertexColor[4][2] = 1.0;
+    vertexColor[5][0] = 0.0; vertexColor[5][1] = 1.0; vertexColor[5][2] = 1.0;
+    vertexColor[6][0] = 0.5; vertexColor[6][1] = 0.5; vertexColor[6][2] = 0.5;
+    vertexColor[7][0] = 0.0; vertexColor[7][1] = 0.0; vertexColor[7][2] = 0.0;
 
-	faceColor[0][0] = 1.0, faceColor[0][1] = 0.0; faceColor[0][2] = 0.0;
-	faceColor[1][0] = 0.0, faceColor[1][1] = 1.0; faceColor[1][2] = 0.0;
-	faceColor[2][0] = 0.0, faceColor[2][1] = 0.0; faceColor[2][2] = 1.0;
-	faceColor[3][0] = 1.0, faceColor[3][1] = 1.0; faceColor[3][2] = 0.0;
-	faceColor[4][0] = 1.0, faceColor[4][1] = 0.0; faceColor[4][2] = 1.0;
-	faceColor[5][0] = 0.0, faceColor[5][1] = 1.0; faceColor[5][2] = 1.0;
-
-	vertexColor[0][0] = 1.0, vertexColor[0][1] = 0.0; vertexColor[0][2] = 0.0;
-	vertexColor[1][0] = 0.0, vertexColor[1][1] = 1.0; vertexColor[1][2] = 0.0;
-	vertexColor[2][0] = 0.0, vertexColor[2][1] = 0.0; vertexColor[2][2] = 1.0;
-	vertexColor[3][0] = 1.0, vertexColor[3][1] = 1.0; vertexColor[3][2] = 0.0;
-	vertexColor[4][0] = 1.0, vertexColor[4][1] = 0.0; vertexColor[4][2] = 1.0;
-	vertexColor[5][0] = 0.0, vertexColor[5][1] = 1.0; vertexColor[5][2] = 1.0;
-	vertexColor[6][0] = 0.5, vertexColor[6][1] = 0.5; vertexColor[6][2] = 0.5;
-	vertexColor[7][0] = 0.0, vertexColor[7][1] = 0.0; vertexColor[7][2] = 0.0;
-
-    vertexNormal[0][0] = -1; vertexNormal[0][1] = -1; vertexNormal[0][2] = -1;
-    vertexNormal[1][0] = -1; vertexNormal[1][1] = 1;  vertexNormal[1][2] = -1;
-    vertexNormal[2][0] = 1;  vertexNormal[2][1] = 1;  vertexNormal[2][2] = -1;
-    vertexNormal[3][0] = 1;  vertexNormal[3][1] = -1; vertexNormal[3][2] = -1;
-    vertexNormal[4][0] = -1; vertexNormal[4][1] = -1; vertexNormal[4][2] = 1;
-    vertexNormal[5][0] = -1; vertexNormal[5][1] = 1;  vertexNormal[5][2] = 1;
-    vertexNormal[6][0] = 1;  vertexNormal[6][1] = 1;  vertexNormal[6][2] = 1;
-    vertexNormal[7][0] = 1;  vertexNormal[7][1] = -1; vertexNormal[7][2] = 1;
+    // Update vertex normals for the card-like rectangle (assuming flat surface)
+    // These normals are simplified assuming a flat surface
+    vertexNormal[0][0] = 0.0; vertexNormal[0][1] = 0.0; vertexNormal[0][2] = 1.0;
+    vertexNormal[1][0] = 0.0; vertexNormal[1][1] = 0.0; vertexNormal[1][2] = 1.0;
+    vertexNormal[2][0] = 0.0; vertexNormal[2][1] = 0.0; vertexNormal[2][2] = 1.0;
+    vertexNormal[3][0] = 0.0; vertexNormal[3][1] = 0.0; vertexNormal[3][2] = 1.0;
+    vertexNormal[4][0] = 0.0; vertexNormal[4][1] = 0.0; vertexNormal[4][2] = 1.0;
+    vertexNormal[5][0] = 0.0; vertexNormal[5][1] = 0.0; vertexNormal[5][2] = 1.0;
+    vertexNormal[6][0] = 0.0; vertexNormal[6][1] = 0.0; vertexNormal[6][2] = 1.0;
+    vertexNormal[7][0] = 0.0; vertexNormal[7][1] = 0.0; vertexNormal[7][2] = 1.0;
 
     r = 1.0;
     g = 0.0;
