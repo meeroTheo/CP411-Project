15e9edf50406be629afb5c0ef64b3cab82c56f42
Thenu1200 <thenukan1200@gmail.com>
2023-12-01 14:08:58 -0500

    Update Board.cpp

--- src/Board.cpp
+++ src/Board.cpp
@@ -8,7 +8,6 @@
 #include "Board.hpp"
 
 Board::Board(int rows, int cols, float tileSize) {
-    // Initialize the board parameters and size of tiles
     this->rows = rows;
     this->cols = cols;
     this->tileSize = tileSize;
@@ -19,7 +18,6 @@ Board::Board(int rows, int cols, float tileSize) {
 
 void Board::initializeBoard(const char* frontTextureFile, const char* backTextureFile) {
     // Load textures for the tiles
-    // Loop through the board and initialize each tile with front and back textures
     for (int i = 0; i < rows; ++i) {
         for (int j = 0; j < cols; ++j) {
             tiles[i][j].loadTextures(frontTextureFile, backTextureFile);
@@ -31,21 +29,35 @@ void Board::draw(float startX, float startY, float startZ) {
     float x = startX;
     float y = startY;
 
-    // Loop through the board and draw each tile at its position
+    // Draw each tile at its position
     for (int i = 0; i < rows; ++i) {
         for (int j = 0; j < cols; ++j) {
             tiles[i][j].draw(x, y, startZ);
-            x += tileSize; // Move to the next column
+            x += tileSize;
         }
-        x = startX; // Reset X position for the next row
-        y += tileSize; // Move to the next row
+        x = startX;
+        y += tileSize;
     }
 }
 
 void Board::handleMouseClick(float mouseX, float mouseY) {
     // Check which tile the mouse click corresponds to
-    // Loop through the board and check if the mouse click is inside any tile
-    // If yes, perform the appropriate action (e.g., flip the tile)
+    float startX = /* calculate starting X position */;
+    float startY = /* calculate starting Y position */;
+
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            if (tiles[i][j].isPointInside(mouseX - startX, mouseY - startY)) {
+                // Perform tile action (e.g., flip the tile)
+                tiles[i][j].flip();
+                // Additional logic can be added based on the game's requirements
+                return; // Exit loop after finding the clicked tile
+            }
+            startX += tileSize;
+        }
+        startX = /* calculate starting X position */;
+        startY += tileSize;
+    }
 }
 
 
