0fe295cdf9b0292a3993a8fe6bba6ae4b0118790
Thenu1200 <thenukan1200@gmail.com>
2023-12-01 14:05:07 -0500

    Update Tile.cpp

--- src/Tile.cpp
+++ src/Tile.cpp
@@ -6,44 +6,70 @@
  */
 // Tile.cpp
 #include "Tile.hpp"
+#include <GL/glut.h> // Include OpenGL headers or libraries for texture loading
 
 Tile::Tile() {
-    // Initialize variables, load textures, etc.
     isFlipped = false;
 }
 
 void Tile::loadTextures(const char* frontTextureFile, const char* backTextureFile) {
-    // Load textures for front and back sides of the tile
-    // Use OpenGL functions or libraries to load textures
-    // Example: Load frontTexture and backTexture using SOIL, stb_image, etc.
+    // Example of texture loading using GLUT library (modify according to your texture loading method)
+    // Load front side texture
+    frontTexture = LoadTexture(frontTextureFile);
+
+    // Load back side texture
+    backTexture = LoadTexture(backTextureFile);
 }
 
 void Tile::draw(float x, float y, float z) {
     glPushMatrix();
-    // Translate to the specified position (x, y, z)
     glTranslatef(x, y, z);
 
-    // Draw the tile based on the flip state
     if (isFlipped) {
-        // Draw the back side of the tile
-        // Use backTexture and draw a textured quad
+        glEnable(GL_TEXTURE_2D);
+        glBindTexture(GL_TEXTURE_2D, backTexture);
+        glBegin(GL_QUADS);
+        // Draw back side quad with texture coordinates
+        glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f, 0.0f);
+        glTexCoord2f(1.0f, 0.0f); glVertex3f(1.0f, -1.0f, 0.0f);
+        glTexCoord2f(1.0f, 1.0f); glVertex3f(1.0f, 1.0f, 0.0f);
+        glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f, 1.0f, 0.0f);
+        glEnd();
+        glDisable(GL_TEXTURE_2D);
     } else {
-        // Draw the front side of the tile
-        // Use frontTexture and draw a textured quad
+        glEnable(GL_TEXTURE_2D);
+        glBindTexture(GL_TEXTURE_2D, frontTexture);
+        glBegin(GL_QUADS);
+        // Draw front side quad with texture coordinates
+        glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f, 0.0f);
+        glTexCoord2f(1.0f, 0.0f); glVertex3f(1.0f, -1.0f, 0.0f);
+        glTexCoord2f(1.0f, 1.0f); glVertex3f(1.0f, 1.0f, 0.0f);
+        glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f, 1.0f, 0.0f);
+        glEnd();
+        glDisable(GL_TEXTURE_2D);
     }
 
     glPopMatrix();
 }
 
 bool Tile::isPointInside(float x, float y) {
-    // Check if a point (x, y) is inside the tile's bounds
-    // Return true if the point is inside, false otherwise
-    // Useful for handling mouse clicks on tiles
+    // Placeholder logic for point inside check
+    return (x >= -1.0f && x <= 1.0f && y >= -1.0f && y <= 1.0f);
 }
 
 void Tile::flip() {
-    // Method to flip the tile from its current state
     isFlipped = !isFlipped;
+}
+
+// Example texture loading function (using GLUT)
+GLuint Tile::LoadTexture(const char* textureFile) {
+    GLuint textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    // Load and set texture parameters here using the specified textureFile
+    // Example: Use the SOIL library, stb_image, or other texture loading methods
+    // Ensure to handle image data loading, format, and parameters correctly
+    return textureID;
 }
 
 
