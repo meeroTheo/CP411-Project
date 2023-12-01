5203188bd6a76009e409d63b1cad8446fedf9fec
Thenu1200 <thenukan1200@gmail.com>
2023-12-01 14:12:21 -0500

    Update GameLogic.cpp

--- src/GameLogic.cpp
+++ src/GameLogic.cpp
@@ -9,19 +9,48 @@
 
 GameLogic::GameLogic() {
     // Initialize the game logic, create the board, set up tiles, etc.
+    gameBoard = Board(4, 4, TILE_SIZE);
+    gameBoard.initializeBoard("frontTexture.png", "backTexture.png");
+    isFlippingAllowed = true; // Assume flipping is allowed at the start
 }
 
 void GameLogic::startGame() {
     // Logic to start the game
-    // May involve shuffling tiles, setting up the initial game state, etc.
+    gameBoard.initializeBoard("frontTexture.png", "backTexture.png");
+    isFlippingAllowed = true; // Reset flipping allowance
 }
 
-void GameLogic::handleTileSelection(int tileID) {
+void GameLogic::handleTileSelection(int row, int col) {
     // Logic to handle tile selection
-    // May involve flipping the tile, checking for matches, etc.
-    // You may interact with the Board object to perform these actions
-    gameBoard.flipTile(tileID);
-    // Add more logic as needed
+    if (isFlippingAllowed) {
+        int tileID = gameBoard.getTileID(row, col);
+        gameBoard.flipTile(row, col); // Flip the selected tile
+
+        // Perform further actions based on the flipped tile
+        if (selectedTile.isFlipped()) {
+            // Handle logic when the second tile is flipped
+            // Check for match, update game state, etc.
+            if (selectedTile.matches(gameBoard.getTile(row, col))) {
+                // Match found
+                // Do something, update score, etc.
+            } else {
+                // No match
+                // Flip the tiles back after a delay or another interaction
+            }
+            // Reset selectedTile to null or an empty state
+            selectedTile.reset();
+        } else {
+            // Handle logic when the first tile is flipped
+            // Update selectedTile to keep track of the first flipped tile
+            selectedTile = gameBoard.getTile(row, col);
+        }
+    }
+}
+
+void GameLogic::endGame() {
+    // Logic to end the game
+    isFlippingAllowed = false; // Disallow flipping
+    // Calculate final score, show game-over screen, etc.
 }
 
 
